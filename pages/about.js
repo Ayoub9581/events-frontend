809ec774c3b8e02a669d967280b1d785bbad3ae1
ayoub ar <ayoubarahmat9@gmail.com>
2021-07-06 21:05:34 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1> this is about page </h1>
+      <p>this is an app to find the latest DJ and other musical events</p>
+    </div>
+  );
+};
+
+export default AboutPage;
