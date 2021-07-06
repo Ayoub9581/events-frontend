c051ceafc3e468224fa023f3f3fc5d74e917214f
ayoub ar <ayoubarahmat9@gmail.com>
2021-07-06 22:26:46 +0100

    module alias with jsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,16 @@
+import { FaExclamationTriangle } from 'react-icons/fa';
 import Link from 'next/link';
-import Layout from './../components/Layout';
-import styles from '../styles/404.module.css';
+import Layout from '@/components/Layout';
+import styles from '@/styles/404.module.css';
 const NotFoundPage = () => {
   return (
     <Layout title="page not found">
       <div className={styles.error}>
-        <h1>404</h1>
+        <h1>
+          {' '}
+          <FaExclamationTriangle />
+          404
+        </h1>
         <h1>Sorry there is nothing here</h1>
         <Link href="/"> Go Back home</Link>
       </div>
